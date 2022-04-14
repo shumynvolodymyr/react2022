e4bc46fe2cc9ca31471a85245bc9f4eb048f8b6f
shumynvolodymyr <shumivova@gmail.com>
2022-04-14 22:57:26 +0300

    hw 3 Done

--- src/validators/car.validators.js
+++ src/validators/car.validators.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const carValidator = Joi.object({
-    model: Joi.string().min(1).max(20).required(),
+    model: Joi.string().regex(/^[a-zA-ZА-яёЁіІїЇ]{1,20}$/).required(),
     price: Joi.number().min(0).max(1000000).required(),
     year: Joi.number().min(1990).max(new Date().getFullYear()).required(),
 });
